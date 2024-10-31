a25ec57aaea0690f04f5dbdf73b1586a819e1662
hee082024kh <hee082024@gmail.com>
2024-10-31 19:48:17 +0100

    Added pavilion.cs

--- classes/locations.cs
+++ classes/locations.cs
@@ -8,6 +8,7 @@ public class Locations
     ShopLocation shop = new();
     ArtGallery art = new();
     OfficeBuilding office = new();
+    PavilionLocation pavilion = new();
     public string? currentLocation;
     public int? input;
     public void LocationSelector()
@@ -50,7 +51,7 @@ public class Locations
                 break;
             case 7:
                 currentLocation = "Pavilion";
-                Pavilion();
+                pavilion.Pavilion();
                 break;
             case 8:
                 currentLocation = "Abandoned Warehouse";
@@ -78,56 +79,7 @@ public class Locations
         }
     }
 
-    public void Pavilion()
-    {
-        currentLocation = "Pavilion";
-        Console.Clear();
-        Console.WriteLine($"You are at the  -{currentLocation}-");
-        Thread.Sleep(500);
-        Console.WriteLine("---What do you want to do?---");
-        Console.WriteLine("1. Travel to a different location");
-        Console.WriteLine("2. Check inventory");
-        Console.WriteLine("3. Exit through the gate");
-        Console.WriteLine("4. Look around for anything useful");
-        try
-        {
-            input = Convert.ToInt32(Console.ReadLine());
-        }
-        catch
-        {
-        }
-        switch (locations.input)
-        {
-            case 1:
-                {
-                    locations.LocationSelector();
-                    break;
-                }
-            case 2:
-                {
-                    Console.Clear();
-                    Console.WriteLine("Press -enter- to go back");
-                    inventory.openInventory();
-                    Console.ReadLine();
-                    break;
-                }
-            case 3:
-                {
-                    break;
-                }
-            case 4:
-                {
-                    break;
-                }
-            default:
-                {
-                    Console.Clear();
-                    Console.Write("---Invalid input---");
-                    Thread.Sleep(1000);
-                    break;
-                }
-        }
-    }
+
     public void AbandonedWarehouse()
     {
         currentLocation = "AbandonedWarehouse";
