a9d7761dd2d76a4efcecdca05719c3c49fd61e9b
hee082024kh <hee082024@gmail.com>
2024-10-31 19:52:36 +0100

    Added shelter.cs

--- classes/locations.cs
+++ classes/locations.cs
@@ -10,6 +10,7 @@ public class Locations
     OfficeBuilding office = new();
     PavilionLocation pavilion = new();
     AbandonedWarehouse warehouse = new();
+    BombShelter shelter = new();
     public string? currentLocation;
     public int? input;
     public void LocationSelector()
@@ -60,7 +61,7 @@ public class Locations
                 break;
             case 9:
                 currentLocation = "Bomb Shelter";
-                BombShelter();
+                shelter.Shelter();
                 break;
             case 10:
                 currentLocation = "Fountain";
@@ -80,56 +81,6 @@ public class Locations
         }
     }
 
-    public void BombShelter()
-    {
-        currentLocation = "BombShelter";
-        Console.Clear();
-        Console.WriteLine($"You are at the  -{currentLocation}-");
-        Thread.Sleep(500);
-        Console.WriteLine("---What do you want to do?---");
-        Console.WriteLine("1. Travel to a different location");
-        Console.WriteLine("2. Check inventory");
-        Console.WriteLine("3. Exit through the gate");
-        Console.WriteLine("4. Look around for anything useful");
-        try
-        {
-            input = Convert.ToInt32(Console.ReadLine());
-        }
-        catch
-        {
-        }
-        switch (locations.input)
-        {
-            case 1:
-                {
-                    locations.LocationSelector();
-                    break;
-                }
-            case 2:
-                {
-                    Console.Clear();
-                    Console.WriteLine("Press -enter- to go back");
-                    inventory.openInventory();
-                    Console.ReadLine();
-                    break;
-                }
-            case 3:
-                {
-                    break;
-                }
-            case 4:
-                {
-                    break;
-                }
-            default:
-                {
-                    Console.Clear();
-                    Console.Write("---Invalid input---");
-                    Thread.Sleep(1000);
-                    break;
-                }
-        }
-    }
     public void Fountain()
     {
         currentLocation = "Fountain";
