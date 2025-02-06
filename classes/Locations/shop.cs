f76dfe3c2079243360fac22a102cede3c7f40714
HEE082024KH <hee082024@gmail.com>
2025-02-06 13:18:47 +0100

    Added event to Shop

--- classes/Locations/shop.cs
+++ classes/Locations/shop.cs
@@ -3,6 +3,7 @@ public class ShopLocation(Locations locations)
   private readonly Items items = locations.items;
   private readonly Locations locations = locations;
   private readonly Status status = new(locations, locations.items);
+  private bool shopOutside;
 
   public void Shop()
   {
@@ -47,39 +48,53 @@ public class ShopLocation(Locations locations)
       }
       case 4:
       {
-        Console.Write("You walk around outside the shop");
-        Thread.Sleep(2500);
-        Console.WriteLine(", looking for anything useful.");
+        if (!shopOutside)
+        {
+          Console.Write("You walk around outside the shop");
+          Thread.Sleep(2500);
+          Console.WriteLine(", looking for anything useful.");
+          Thread.Sleep(2000);
+          Console.WriteLine("Around the back you find a few bags of trash.");
+          Thread.Sleep(2500);
+          Console.WriteLine("");
+          shopOutside:
+          Console.WriteLine("--What do you want to do?--");
+          Console.WriteLine("1. Search the bags");
+          Console.WriteLine("2. Leave them alone");
+          try
+          {
+            locations.input = Convert.ToInt32(Console.ReadLine());
+          }
+          catch
+          {
+            Console.Clear();
+            Console.WriteLine("Invalid input");
+            Thread.Sleep(1500);
+            goto shopOutside;
+          }
+
+          switch (locations.input)
+          {
+            case 1:
+              Console.WriteLine("You search the bags for anything useful.");
+              Thread.Sleep(2000);
+              Console.Write("To your surprise you find some loose change");
+              Thread.Sleep(2000);
+              Console.WriteLine(", about 5 bucks.");
+              Thread.Sleep(2000);
+              items.Money += 5;
+              shopOutside = true;
+              Shop();
+              break;
+            case 2:
+              Shop();
+              break;
+          }
+        }
+
+        Console.WriteLine("There is nothing more to find here.");
         Thread.Sleep(2000);
-        Console.WriteLine("Around the back you find a few bags of trash.");
-        Thread.Sleep(2500);
-        Console.WriteLine("");
-        shopOutside:
-        Console.WriteLine("--What do you want to do?--");
-        Console.WriteLine("1. Search the bags");
-        Console.WriteLine("2. Leave them alone");
-        try
-        {
-          locations.input = Convert.ToInt32(Console.ReadLine());
-        }
-        catch
-        {
-          Console.Clear();
-          Console.WriteLine("Invalid input");
-          Thread.Sleep(1500);
-          goto shopOutside;
-        }
-
-        switch (locations.input)
-        {
-          case 1:
-            ;
-            break;
-          case 2:
-            ;
-            break;
-        }
-
+        Shop();
         break;
       }
       default:
