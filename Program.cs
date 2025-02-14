ee7188051cbe878780c4da1cc8db57b36c40da2b
HEE082024KH <hee082024@gmail.com>
2025-02-14 12:31:52 +0100

    Minor changes

--- Program.cs
+++ Program.cs
@@ -2,8 +2,10 @@
 
 class Program
 {
-  static void Main(Controller controller)
+  static void Main()
   {
+    Controller controller = new();
+
     // Run Game
     Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
     controller.Run();
