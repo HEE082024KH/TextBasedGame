afb6c46a3d3a31c90aa65a0b4f8e17096de3a391
HEE082024KH <hee082024@gmail.com>
2025-02-19 09:07:45 +0100

    Deactivated fullscreen for testing

--- Program.cs
+++ Program.cs
@@ -5,20 +5,20 @@ namespace TextBasedGame;
 
 class Program
 {
-  [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
-  static extern IntPtr GetConsoleWindow();
-
-  [DllImport("user32.dll")]
-  static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-
-  const int SwMaximize = 5;
+  // [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+  // static extern IntPtr GetConsoleWindow();
+  //
+  // [DllImport("user32.dll")]
+  // static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+  //
+  // const int SwMaximize = 5;
 
   static void Main()
   {
     Controller controller = new();
-
-    ShowWindow(GetConsoleWindow(), SwMaximize);
-    Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+    //
+    // ShowWindow(GetConsoleWindow(), SwMaximize);
+    // Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
 
     // Run Game
     controller.Run();
